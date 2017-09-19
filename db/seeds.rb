5c638ae8e5fef51a4648f733a372168bc7ef755c
yuji91_1227 <yuji91.1227@gmail.com>
2017-09-19 13:17:32 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 # coding: utf-8
 Blog.create(:title => 'title_01', :content => 'content_01')
+Blog.create(:title => 'title_02', :content => 'content_02')
+Blog.create(:title => 'title_03', :content => 'content_03')
 Poem.create(:title => 'title_01', :content => 'content_01', :author => 'author_01')
+Poem.create(:title => 'title_02', :content => 'content_02', :author => 'author_02')
+Poem.create(:title => 'title_03', :content => 'content_03', :author => 'author_03')
